fc16a77f294824947d37bb3b53dc4e74bfedd8a1
hadson0 <hadsonsb2221@gmail.com>
2022-11-04 20:01:48 -0300

    Melhorias

--- main.cpp
+++ main.cpp
@@ -1,27 +1,8 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-vector<vector<string>> cyk;
-map<string, string> pd;
+vector<vector<string>> cykTable;
 map<string, vector<string>> grammar;
-
-// Armazena a gramatica dada no input
-void getGrammar() {
-    //int n;
-    //cin >> n;
-
-    //while (n--) {
-    //    string a, b;
-    //    grammar[a].push_back(b);
-    //}
-
-    grammar["S"] = vector<string>({"AX", "AB"});
-    grammar["R"] = vector<string>({"AX", "AB"});
-    grammar["X"] = vector<string>({"RB", "b"});
-    grammar["A"] = vector<string>({"a"});
-    grammar["B"] = vector<string>({"b"});   
-}
 
 // Verifica se a string c é produto cartesiano das strings a e b
 bool isCartesianProduct(string a, string b, string c) {
@@ -29,11 +10,12 @@ bool isCartesianProduct(string a, string b, string c) {
         string product = string() + a[i / b.size()] + b[i % b.size()];
         if (product == c) return true;
     }
-
     return false;
 }
 
-void dp(string w) {
+void CYK(string w) {
+    map<string, string> pd;
+    cykTable = vector<vector<string>> (w.length(), vector<string> (w.length(), ""));
     for (int ns = 1, n = w.length(); ns <=  n; ns++) {  // Tamanho do conjunto de letras  
         for (int i = 0; i <= n - ns; i++) { // Itera pelos conjuntos possiveis
             string subW = w.substr(i, ns); // Conjunto 
@@ -46,11 +28,10 @@ void dp(string w) {
                     for (int k = 1, wn = subW.length(); k <= wn; k++) { // Controla as combinacoes do conjunto
                         try { // As vezes a subW eh pequena e quebra na funcao isCartesianProduct, poderia ser resolvido com ifs, mas assim fica menor                   
                             if (!subW.compare(b[j]) or isCartesianProduct(pd[subW.substr(0, k)], pd[subW.substr(k, wn - k)], b[j])) {
-                                if (pd[subW].find(a) == string::npos) {            
+                                if (pd[subW].find(a) == string::npos) {  // Caso o simbolo nao estiver na pd, o insere          
                                     pd[subW] = (pd[subW] == "") ? a : pd[subW] + a;
                                 }
-
-                                cyk[ns - 1][i] = pd[subW];
+                                cykTable[ns - 1][i] = pd[subW];
                             }
                         } catch(...) {}
                     }
@@ -60,14 +41,19 @@ void dp(string w) {
     }    
 }
 
-int main() {    
-    getGrammar();
+int main() {
+    string w, symb, aux, term;
+    int n;
+    cin >> n;
 
-    string w;
-    while (getline(std::cin, w) and w.compare("*")) {
-        cyk = vector<vector<string>> (w.length(), vector<string> (w.length(), ""));  
-        dp(w);
-        cout << w << ": " << ((cyk[w.length() - 1][0].find("S") != string::npos) ? "SIM" : "NAO") << endl;
+    while (n--) { 
+        cin >> symb >> aux >> term;
+        grammar[symb].push_back(term);
+    }
+
+    while (getline(cin >> ws, w) and w.compare("*")) {         
+        CYK(w);
+        cout << w << ": " << ((cykTable[w.length() - 1][0].find("S") != string::npos) ? "SIM" : "NAO") << endl;
     }
 
     return 0;
